39abf6e225509d927b2b8c8f198082c9da471342
Denis Rocher <denis.rocher@gmail.com>
2021-01-15 14:55:54 -0300

    Inicio sección curso JS

---
+++ hola-mundo/js/hola_mundo.js
@@ -0,0 +1,7 @@
+/*
+alert("Hola Mundo JS");
+alert("Bienvenido al curso de JS");
+*/
+//document.write("Hola mundo desde archivo .js externo");
+//console.log("muestra esto en la consola");
+//console.log(88+4);
